e0cc9da534e55615af1bffa50b75e9b78fd969d5
Youssef Bahgat <62761039+itsbahgat@users.noreply.github.com>
2025-11-18 01:05:44 +0200

    feat: integrate HeroUI and add theme colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
-import './global.css';
-import 'react-native-gesture-handler';
-import { Navigation } from './navigation';
+import "./global.css";
+import "react-native-gesture-handler";
+import { Navigation } from "./navigation";
 import {
   useFonts,
   DMSans_400Regular,
   DMSans_600SemiBold,
   DMSans_700Bold,
-} from '@expo-google-fonts/dm-sans';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+} from "@expo-google-fonts/dm-sans";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import { HeroUINativeProvider } from "heroui-native";
 
 SplashScreen.preventAutoHideAsync();
 
 export function App() {
   const [fontsLoaded] = useFonts({
-    'DMSans-Regular': DMSans_400Regular,
-    'DMSans-SemiBold': DMSans_600SemiBold,
-    'DMSans-Bold': DMSans_700Bold,
+    "DMSans-Regular": DMSans_400Regular,
+    "DMSans-SemiBold": DMSans_600SemiBold,
+    "DMSans-Bold": DMSans_700Bold,
   });
 
   useEffect(() => {
@@ -29,5 +30,9 @@ export function App() {
     return null;
   }
 
-  return <Navigation />;
+  return (
+    <HeroUINativeProvider>
+      <Navigation />
+    </HeroUINativeProvider>
+  );
 }
