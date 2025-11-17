53f98e33bad0134a5aec73dff1a3b78672fdd63a
Youssef Bahgat <62761039+itsbahgat@users.noreply.github.com>
2025-11-17 23:40:34 +0200

    feat: add DMSans font and update global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
 import './global.css';
 import 'react-native-gesture-handler';
 import { Navigation } from './navigation';
+import {
+  useFonts,
+  DMSans_400Regular,
+  DMSans_600SemiBold,
+  DMSans_700Bold,
+} from '@expo-google-fonts/dm-sans';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+
+SplashScreen.preventAutoHideAsync();
 
 export function App() {
+  const [fontsLoaded] = useFonts({
+    'DMSans-Regular': DMSans_400Regular,
+    'DMSans-SemiBold': DMSans_600SemiBold,
+    'DMSans-Bold': DMSans_700Bold,
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return <Navigation />;
 }
