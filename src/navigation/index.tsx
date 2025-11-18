11cccee65c4469f1ae004eaed82849fc8c939cbe
Youssef Bahgat <62761039+itsbahgat@users.noreply.github.com>
2025-11-18 13:37:26 +0200

    feat: add navigation for appointment booking screens

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,10 +1,27 @@
-import { createStaticNavigation } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './screens/Home';
+import { createStaticNavigation } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import BookingConfirmationScreen from "../features/appointments/screens/booking-confirmation-screen";
+import BookAppointmentScreen from "@/features/appointments/screens/book-appointment-screen";
+
+export type RootStackParamList = {
+  BookAppointment: undefined;
+  BookingConfirmation: undefined;
+};
 
 const RootStack = createNativeStackNavigator({
   screens: {
-    Home: HomeScreen,
+    BookAppointment: {
+      screen: BookAppointmentScreen,
+      options: {
+        headerShown: false,
+      },
+    },
+    BookingConfirmation: {
+      screen: BookingConfirmationScreen,
+      options: {
+        title: "Booking Confirmation",
+      },
+    },
   },
 });
 
